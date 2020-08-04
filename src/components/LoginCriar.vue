0974d907d4c6c3c802ef1a25c057df93687c2cf2
Rodolfo Santos <rodolfodossantos29@gmail.com>
2020-08-04 20:55:21 -0300

    Adicionando Formulário de Criação de Conta

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,10 +1,41 @@
 <template>
-  <p>Login Criar</p>
+  <section>
+    <h2>Crie a sua Conta</h2>
+    <transition mode="out-in">
+      <button v-if="!criar" class="btn" @click="criar = true">Criar Conta</button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar Usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
 </template>
-
 <script>
-export default {};
+import UsuarioForm from "@/components/UsuarioForm.vue";
+export default {
+  components: {
+    UsuarioForm,
+  },
+  name: "LoginCriar",
+
+  data() {
+    return {
+      criar: false,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
---
+++ src/components/UsuarioForm.vue
@@ -0,0 +1,99 @@
+<template>
+  <form>
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" v-model="nome" />
+
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" v-model="email" />
+
+    <label for="senha">Senha</label>
+    <input type="password" name="senha" id="senha" v-model="senha" />
+
+    <label for="cep">Cep</label>
+    <input type="text" name="cep" id="cep" v-model="cep" @keyup="preecherCep" />
+
+    <label for="rua">Rua</label>
+    <input type="text" name="rua" id="rua" v-model="rua" />
+
+    <label for="numero">Nº</label>
+    <input type="text" name="numero" id="numero" v-model="numero" />
+
+    <label for="bairro">Bairro</label>
+    <input type="text" name="bairro" id="bairro" v-model="bairro" />
+
+    <label for="cidade">Cidade</label>
+    <input type="text" name="cidade" id="cidade" v-model="cidade" />
+
+    <label for="estado">Estado</label>
+    <input type="text" name="estado" id="estado" v-model="estado" />
+    <div class="button">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<script>
+import { mapFields } from "@/helpers";
+import { getCep } from "@/services/cep";
+
+export default {
+  name: "UsuarioForm",
+  computed: {
+    ...mapFields({
+      fields: [
+        "nome",
+        "email",
+        "senha",
+        "cep",
+        "rua",
+        "numero",
+        "bairro",
+        "cidade",
+        "estado",
+      ],
+      base: "usuario",
+      mutation: "UPDATE_USUARIO",
+    }),
+
+    // nome: {
+    //   get() {
+    //     return this.$store.state.usuario.nome;
+    //   },
+    //   set(value) {
+    //     this.$store.commit("UPDATE_USUARIO", { nome: value });
+    //   },
+    // },
+  },
+
+  methods: {
+    preecherCep() {
+      const cep = this.cep.replace(/\D/g, "");
+      if (cep.length === 8) {
+        getCep(cep).then((response) => {
+          this.rua = response.data.logradouro;
+          this.bairro = response.data.bairro;
+          this.cidade = response.data.localidade;
+          this.estado = response.data.uf;
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  align-items: center;
+}
+
+.button {
+  grid-column: 2;
+  margin-top: 10px;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+</style>
